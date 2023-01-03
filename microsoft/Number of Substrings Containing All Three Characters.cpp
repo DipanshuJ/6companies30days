858893c1e0f5f2f5193ca1bb8a97f9f5d7121add
DipanshuJ <dipanshuj09@gmail.com>
2023-01-03 13:06:09 +0530

    initial comit

---
+++ microsoft/Number of Substrings Containing All Three Characters.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define f(i,a,b) for(i=a;i<b;i++)
+#define fi(i,a,b) for(int i=a;i<b;i++)
+#define fd(i,a,b) for(i=a;i>=b;i--)
+#define fdi(i,a,b) for(int i=a;i>=b;i--)
+#define all(x) x.begin(),x.end()
+#define imin INT_MIN
+#define imax INT_MAX
+#define llmin LONG_LONG_MIN
+#define llmax LONG_LONG_MAX
+#define sz(a) a.size()
+ 
+
+void solve(){
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int test=1;
+	cin>>test;
+	while(test--) solve();
+	return 0;
+}
